429acbd9a7dc58ef68c667a42e04709faa1efe5b
weiwb5 <weiwb5@qq.com>
2023-05-05 22:13:10 +0800

    测试

--- IDEAproject/src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ IDEAproject/src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -3,7 +3,7 @@ package com.example.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//核心注解
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
