21502e817644e7e7671276c04401a7011b085ac3
weiwb5 <weiwb5@qq.com>
2023-05-05 22:23:58 +0800

    测试

--- IDEAproject/src/main/java/com/example/springbootdemo/Application.java
+++ IDEAproject/src/main/java/com/example/springbootdemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//核心注解
-public class SpringbootdemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootdemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
